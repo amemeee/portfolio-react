2b7e2697d14c51923ec53e98c22c9b846e7f47fe
amemeee <amishot4@gmail.com>
2023-11-20 07:28:50 +0800

    Update intro.js

--- src/intro.js
+++ src/intro.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
+import classNames from 'classnames';
 
 const Intro = () => {
+  useEffect(() => {
+    const img = document.querySelector('.img-fluid');
+    img.classList.add('animate__animated', 'animate__zoomIn');
+
+    return () => {
+      // Optional cleanup - remove the animation class when component unmounts
+      img.classList.remove('animate__animated', 'animate__zoomIn');
+    };
+  }, []);
+
   return (
     <div className="Intro">
       <Container className="mt-5">
         <Row>
           <Col md={7}>
             {/* Big size image on the left */}
-            <img src="https://media.tenor.com/UQESusJVIRwAAAAd/aizen-aizen-form.gif" alt="Big Image" className="img-fluid rounded" />
+            <img
+              src="https://media.tenor.com/UQESusJVIRwAAAAd/aizen-aizen-form.gif"
+              alt="Big Image"
+              className={classNames('img-fluid', 'rounded')}
+            />
           </Col>
           <Col md={5}>
             {/* Explanation on the right side */}
@@ -16,7 +31,7 @@ const Intro = () => {
               <h1><b>HELLO!</b></h1>
               <h6><b>I'M A SELF TAUGHT DEVELOPER. CURRENTLY, I'M FOCUSED ON THE BACKEND.</b></h6>
               <p>
-              Keep coding, keep exploring, and keep pushing the boundaries of what's possible in the backend realm.
+                Keep coding, keep exploring, and keep pushing the boundaries of what's possible in the backend realm.
               </p>
             </div>
           </Col>
