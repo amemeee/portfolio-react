6068039472d4cc2f2e0101b5849d6b0f5260c5c1
amemeee <amishot4@gmail.com>
2024-05-01 00:05:31 +0800

    update

--- src/intro.js
+++ src/intro.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
+import seishin from './images/seishin.gif'; // Import the image
 import './index.css';
 
 const Intro = () => {
@@ -15,22 +16,24 @@ const Intro = () => {
 
   return (
     <div className="Intro">
-      <Container >
-        <Row>
-          <Col >
-            <img src="https://media.tenor.com/UQESusJVIRwAAAAd/aizen-aizen-form.gif" alt="Big Image" className="img-fluid rounded" />
-            {/* Explanation on the right side */}
-            <div>
-              <br></br>
-              <h1><b>HELLO! <span className="myName" >I'M AMIN</span></b></h1>
-              <h6><b>A SELF TAUGHT DEVELOPER. CURRENTLY, I'M FOCUSED ON THE BACKEND.</b></h6>
-              <p>
-                Keep coding, keep exploring, and keep pushing the boundaries of what's possible in the backend realm.
-              </p>
-            </div>
-          </Col>
-        </Row>
-      </Container>
+      <div className="me-card" >
+        <Container >
+          <Row>
+            <Col >
+              {/* Explanation on the right side */}
+            
+                <div >
+                  <img src={seishin} alt="Big Image" className="img-fluid rounded" />
+                  <div class="overlay-content">
+                    <h1>Your Text</h1>
+                    <button><a href='#' > Button</a></button>
+                  </div>
+                </div>
+             
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </div>
   );
 };
